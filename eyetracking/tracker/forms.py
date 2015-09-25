e725ea30e34be378040673f6f17b5881950283d4
Maner Moniar <maner.moniar@gmail.com>
2015-09-25 00:20:24 -0500

    Se eliminó campo "frame_rate"

--- eyetracking/tracker/forms.py
+++ eyetracking/tracker/forms.py
@@ -7,12 +7,6 @@ class TrackerForm(forms.ModelForm):
                                      error_messages={'required': 'Ip is required.'})
     port = forms.IntegerField(widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Port'}),
                               error_messages={'required': 'Port is required.'})
-    OPTIONS = (
-            (30, "30fps"),
-            (60, "60fps"),
-            )
-    frame_rate = forms.ChoiceField(choices=OPTIONS, widget=forms.Select(attrs= {'class':'form-control'}),
-                                   error_messages={'required': 'Frame rate is required.'})
 
     class Meta:
         model = Tracker
