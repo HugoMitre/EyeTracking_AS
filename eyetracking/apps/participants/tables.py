f4c82ff08a3efa649dfe92a99f028f98160885e2
Maner Moniar <maner.moniar@gmail.com>
2015-11-05 00:52:02 -0600

    Participants package

---
+++ eyetracking/apps/participants/tables.py
@@ -0,0 +1,15 @@
+import django_tables2 as tables
+from django.utils.safestring import mark_safe
+from .models import Participant
+
+
+class ParticipantTable(tables.Table):
+    first_name = tables.Column()
+    actions = tables.TemplateColumn(orderable=False, empty_values=(), template_name='participants/participant_actions.html')
+
+    class Meta:
+        model = Participant
+        fields = ('first_name', 'gender', 'age')
+
+    def render_first_name(self, value, record):
+        return mark_safe('''<a href=%s>%s</a>''' % (record.id, value + ' ' + record.last_name))
