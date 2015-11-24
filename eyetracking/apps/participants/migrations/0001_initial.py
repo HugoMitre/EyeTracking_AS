de6b5100a6f9122b770a91ff99df61b54584f2ff
Maner Moniar <maner.moniar@gmail.com>
2015-11-23 22:39:12 -0600

    Templates of api deleted and new migrations

--- eyetracking/apps/participants/migrations/0001_initial.py
+++ eyetracking/apps/participants/migrations/0001_initial.py
@@ -14,7 +14,10 @@ class Migration(migrations.Migration):
             name='Participant',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(max_length=255, verbose_name=b'Name')),
+                ('first_name', models.CharField(max_length=255, verbose_name=b'Full Name')),
+                ('last_name', models.CharField(max_length=255)),
+                ('age', models.PositiveSmallIntegerField(default=0, verbose_name=b'Age')),
+                ('gender', models.CharField(max_length=1, verbose_name=b'Gender', choices=[(b'M', b'Male'), (b'F', b'Female')])),
             ],
         ),
     ]
